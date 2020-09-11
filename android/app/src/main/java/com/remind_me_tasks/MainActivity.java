5b5b5a5256f7e62974d1988305a63767a11e18bf
Marlo Eichenberg Motta <devmasterjs@programmerhero.com.br>
2020-09-10 22:40:15 -0300

    initial project

---
+++ android/app/src/main/java/com/remind_me_tasks/MainActivity.java
@@ -0,0 +1,15 @@
+package com.remind_me_tasks;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "remind_me_tasks";
+  }
+}
